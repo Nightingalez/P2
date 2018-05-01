5a654f6da6c8951f7b952ea1e0307f47d673a19f
Stefan Nordborg Eriksen <the-grey-side@hotmail.com>
2018-05-01 14:06:35 +0200

    Initial Androird App

---
+++ Android/app/src/main/java/com/b139/testapplication/MainActivity.java
@@ -0,0 +1,13 @@
+package com.b139.testapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
